f5adab7ccaf17fa0344ce65dd3b1203e357f5374
Burt Beckwith <burt@burtbeckwith.com>
2013-04-28 21:30:37 -0400

    added license, issueManagement, and scm properties

--- BreadcrumbsGrailsPlugin.groovy
+++ BreadcrumbsGrailsPlugin.groovy
@@ -6,4 +6,8 @@ class BreadcrumbsGrailsPlugin {
     def title = "Grails Breadcrumbs Plugin"
     def documentation = "http://grails.org/plugin/breadcrumbs"
     def description = 'Provids a taglib for the simple yet customizeable generation of breadcrumbs.'
+
+    def license = 'APACHE'
+    def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPBREADCRUMBS']
+    def scm = [url: 'https://github.com/jamesdh/grails-breadcrumbs']
 }
